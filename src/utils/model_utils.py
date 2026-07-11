19d2af12c4bf0d701ee582e3d0325fc9eb68d3d0
terelopdel-sketch <terelopdel@gmail.com>
2026-07-11 09:21:27 +0200

    añadir función encodear_para_rf a model_utils

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -1,4 +1,5 @@
 import time
+import pandas as pde
 from sklearn.model_selection import cross_validate
 
 def evaluar_modelo(modelo, X, y, cv, scorers, nombre_modelo, nombre_features):
@@ -17,3 +18,12 @@ def evaluar_modelo(modelo, X, y, cv, scorers, nombre_modelo, nombre_features):
         'R2_std'    : round(cv_results['test_R2'].std(), 4),
         'Tiempo_seg': round(fin - inicio, 2)
     }
+
+def encodear_para_rf(train_df, test_df, features, features_categoricas):
+    cat_feats = [f for f in features if f in features_categoricas]
+
+    tr_enc = pd.get_dummies(train_df[features], columns=cat_feats, drop_first=True)
+    te_enc = pd.get_dummies(test_df[features], columns=cat_feats, drop_first=True)
+    te_enc = te_enc.reindex(columns=tr_enc.columns, fill_value=0)
+
+    return tr_enc, te_enc, cat_feats
